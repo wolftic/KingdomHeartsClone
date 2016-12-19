8fb026b022a45e50a1d8179eaa82faaed7d3c6a2
epicacid <noah@Noahs-MacBook-Pro.local>
2016-12-19 19:51:52 +0100

    Created Maps + Movement Script

---
+++ Project/Assets/Noah/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField]
+	private Transform target;
+	[SerializeField]
+	private float dist;
+	private Vector3 angle = Vector3.up;
+	private Transform thisTransform = null;
+	[SerializeField]
+	private float movSpeed = 10.0f;
+	[SerializeField]
+	private float rotSpeed;
+	private float horz;
+	private float vert;
+	void Start () {
+		thisTransform = GetComponent<Transform> ();
+		rotSpeed = 20.0f;
+	}
+	void Update () {
+		transform.LookAt (target.position);
+		horz = Input.GetAxis ("Horizontal");
+		vert = Input.GetAxis ("Vertical");
+		thisTransform.position += transform.forward * vert * Time.deltaTime * movSpeed;
+		transform.RotateAround (target.position, angle, horz * rotSpeed * Time.deltaTime );
+	}
+}
